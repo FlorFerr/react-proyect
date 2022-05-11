78380d905b9fa3a74937a47f258d484d01af2f51
Flor <florferreyra16@gmail.com>
2022-05-11 12:28:10 -0300

    modalCart

--- src/Components/Cart/CartItem.js
+++ src/Components/Cart/CartItem.js
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ItemDetail from '../ItemDetail'
 
 const CartItem = ({data, onRemove}) => {
+
+    const [modalCartShown, setModalCartShown] = useState(false)
+
+
+      const hideModalCartHandler = () =>{
+        setModalCartShown(false)
+      }
+    const showModalCartHandler = () => {     
+        setModalCartShown(true)
+    }
   return (
-     
-        
-                <tr key={data.id}>
-                    <td >{data.name}</td>
-                    <td >x{data.amount}</td>
-                    <td ><button onClick={() => {onRemove(data.id)}}>Remove</button></td>
-                </tr>        
-       
-   
+      <>
+        <tr key={data.id}>
+            <td onClick={showModalCartHandler}>{data.name}</td>
+            <td >x{data.amount}</td>
+            <td  ><button onClick={() => {onRemove(data.id)}}>Remove</button></td>
+        </tr>   
+        {modalCartShown && <ItemDetail onHide={hideModalCartHandler} detail={data}/>}
+        </>
   )}
 
 export default CartItem
