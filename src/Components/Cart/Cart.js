33e0ec28f1a6e043803d64b74804e78732cb43a1
Flor <florferreyra16@gmail.com>
2022-05-23 15:22:46 -0300

    btn Compra en el cart

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,18 +1,28 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import CartContext from '../../Context/CartContext'
 import CartItem from './CartItem'
 import './CartItem.css'
 
 
 const Cart = () => {
+  const [order, setOrder] = useState(false)
     
     const {cartContext, cart} = useContext(CartContext)
+
+    const orderHandler = () => {
+      setOrder(true)
+      cartContext.clearCart()
+    }
     
 
   return (
     <div className='cart-container'>
       <h1>Carrito</h1>
-      { cart.length === 0 ? <p>No hay productos en el carrito</p> :
+     
+        {order ? <p>Compra realizada!</p> :
+         <div>
+      
+      {cart.length === 0 ? <p>No hay productos en el carrito</p> :
       <div className='table-container'>
         {
           cart.map(product=> {
@@ -21,9 +31,11 @@ const Cart = () => {
             )
           })
         }
-        <button onClick={cartContext.clearCart}>Vaciar</button>
+        <button onClick={cartContext.clearCart}>Vaciar carrito</button>
+        <button onClick={orderHandler}>Comprar</button>
       </div>
       }
+      </div>}
     </div>
   
 )}
