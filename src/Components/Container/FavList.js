69e218a4f4af324cc350367f6c9a2255dbb4c98f
Flor <florferreyra16@gmail.com>
2022-07-01 17:01:40 -0300

    filtros favoritos

--- src/Components/Container/FavList.js
+++ src/Components/Container/FavList.js
@@ -1,5 +1,5 @@
-import React, { useContext } from 'react';
-
+import React, { useContext, useState, useEffect } from 'react';
+import SearchItem from './SearchItem'
 import { Link } from 'react-router-dom';
 import FavContext from '../../Context/FavContext';
 import Item from './Item';
@@ -9,8 +9,90 @@ import LoadingSpinner from '../UI/LoadingSpinner';
 
 const FavList = () => {
 
+  const [valueSearch, setValueSearch ] = useState('')
+  const [search, setSearch] = useState([])
+  const [showCategory, setShowCategory] = useState(false)
+  const [categoryParam, setCategoryParam] = useState('')
+  const [noResultSearch, setNoResultaSearch] = useState(false)
+
   const { fav, isLoading } = useContext(FavContext)
-    
+
+  const onSearch = (value) => {
+    setValueSearch(value)
+    setNoResultaSearch(false)
+  }
+
+  useEffect(()=>{
+    setSearch(fav.filter(ele => ele.name.toLowerCase().includes(valueSearch.toLowerCase())))
+    if(search.length === 0){
+      setNoResultaSearch(true)
+    }
+    if(showCategory){
+       setSearch(fav.filter(ele => ele.name.toLowerCase().includes(valueSearch.toLowerCase()) && ele.category === categoryParam))
+    }
+  },[showCategory, valueSearch, fav, search.length, categoryParam])
+
+  const categoriaHandler = (category) => {
+    setShowCategory(true)
+    setCategoryParam(category)
+    setValueSearch('')
+  }
+
+  const allItemsHandler = ()=>{
+    setShowCategory(false)
+  }
+
+return (
+  <div>
+    {fav.length === 0 ? 
+    <div className='fav-noResults'> 
+      <p>No hay favoritos</p> 
+      <Link to='/'><button className='fav-btn_back'><GrLinkPrevious />  Ver productos</button></Link>
+    </div>:
+    <div className='fav-container'>
+      <div className='fav-topbar' >
+        <h2>Mis favoritos</h2>
+        <div className='fav-controls'>
+          <div className='fav-controls_btn'>
+            <button onClick={allItemsHandler}>Todos</button>
+            <button onClick={() =>{categoriaHandler('beer')}}>Cervezas</button>
+            <button onClick={() =>{categoriaHandler('burger')}}>Hamburguesas</button>
+          </div>
+          <SearchItem onSearch={onSearch} value={valueSearch}></SearchItem>
+        </div>
+      </div>
+      {isLoading && <div className='loading'><LoadingSpinner /></div>}
+      <div className='fav-content'>
+      {valueSearch && 
+        search.map((item) =>{
+          return(
+            <Item key={item.name} data={item}/>
+          )
+        })
+      }
+      {showCategory && !valueSearch && search.map(item=> {
+        return(
+          <Item key={item.name} data={item} />
+        )
+      })}
+      {!showCategory && !valueSearch && 
+        fav.map((item) =>{
+          return(
+            <Item key={item.name} data={item}/>
+          )
+        })
+      }
+      {showCategory && search.length === 0 && !valueSearch &&  <p>No hay resultados </p>}
+      {valueSearch && noResultSearch && <p>No hay resultados</p>}
+     </div>
+     </div>}
+  </div>
+)
+}
+
+export default FavList
+
+  /*
   return (
     <div>
       {fav.length === 0 ? 
@@ -38,4 +120,4 @@ const FavList = () => {
   )
 }
 
-export default FavList
+export default FavList*/
