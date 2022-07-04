da46f2daccaf6e482dc3938eeefe5aa41dd6cfc7
Flor <florferreyra16@gmail.com>
2022-07-04 09:38:40 -0300

    fav list

--- src/Components/Container/FavList.js
+++ src/Components/Container/FavList.js
@@ -91,33 +91,3 @@ return (
 }
 
 export default FavList
-
-  /*
-  return (
-    <div>
-      {fav.length === 0 ? 
-      <div className='fav-noResults'> 
-        <p>No hay favoritos</p> 
-        <Link to='/'><button className='fav-btn_back'><GrLinkPrevious />  Ver productos</button></Link>
-      </div>:
-      <div className='fav-container'>
-        <div className='fav-topbar' >
-          <h2>Mis favoritos</h2>
-         
-        </div>
-        {isLoading && <div className='loading'><LoadingSpinner /></div>}
-        <div className='fav-content'>
-      
-        {fav.map((item) =>{
-            return(
-              <Item key={item.name} data={item}/>
-            )
-          })}
-       </div>
-       </div>}
-       
-    </div>
-  )
-}
-
-export default FavList*/
